54158a2688bcffc7f6a8e18c6da76dd515661440
Michael <mlko53@stanford.edu>
2019-05-10 02:01:03 -0700

    code mask A and B

--- models/PixelCNN/masks.py
+++ models/PixelCNN/masks.py
@@ -28,16 +28,27 @@ class ConvMaskB(nn.Conv2d):
 
         out_c, in_c, HH, WW = self.weight.size()
         mask = torch.ones(out_c, in_c, HH, WW)
-        # create mask here
+        mask[:,:, HH // 2, (WW // 2 + 1):] = 0
+        mask[:,:, (HH // 2 + 1):, :] = 0
+
+        self.mask = mask
 
     def forward(self, x):
-        # update weight with mask here
+        self.weight.data *= self.mask
         return super(ConvMaskB, self).forward(x)
 
 
 if __name__ == "__main__":
+    print("Testing Conv Mask A")
     convMaskA = ConvMaskA(1, 1, 7, 1, 0)
     x = torch.ones(1, 1, 7, 7)
     print(convMaskA.weight.data[0,0,:,:])
     print(convMaskA.mask[0,0,:,:])
+    print(convMaskA(x)[0,0,:,:])
+
+    print("Testing Conv Mask B")
+    convMaskB = ConvMaskB(1, 1, 7, 1, 0)
+    x = torch.ones(1, 1, 7, 7)
+    print(convMaskB.weight.data[0,0,:,:])
+    print(convMaskB.mask[0,0,:,:])
     print(convMaskA(x)[0,0,:,:])
