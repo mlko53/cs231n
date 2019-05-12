6f46ab60d346fbcce11979a5f69d5bcf2a720f8e
mlko53 <mlko53@stanford.edu>
2019-05-11 22:00:56 -0700

    fix data

--- dataloader/randomDataset.py
+++ dataloader/randomDataset.py
@@ -7,12 +7,13 @@ class RandomDataset(data.Dataset):
     def __init__(self, split):
         super(RandomDataset, self).__init__()
         self.split = split
+        self.data = torch.randn(1000, 3, 224, 244)
 
     def __len__(self):
         return 1000
 
     def __getitem__(self, index):
-        x = torch.randn(3,224,224)
+        x = self.data[index]
         y = torch.randn(1)
 
         return x, y
